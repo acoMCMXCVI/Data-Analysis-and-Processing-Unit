7c943fe10a06a3938d4e5b7b15c44e25b781f224
Aleksandar Rajic <rajicaleksandar6@gmail.com>
2019-05-28 11:25:48 +0200

    Working on windows.

---
+++ workflow.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+###############################################################################
+# Config.
+
+BUILD_OPENCV=true
+
+###############################################################################
+# Resources.
+
+# http://dlib.net/compile.html
+
+###############################################################################
+# Download
+
+mkdir download
+pushd download
+#TODO
+wget https://github.com/git-for-windows/git/releases/download/v2.21.0.windows.1/Git-2.21.0-64-bit.exe
+wget https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4-win64-x64.msi
+wget http://dlib.net/files/dlib-19.17.zip
+wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+curl -OJ https://codeload.github.com/opencv/opencv/zip/3.2.0
+wget https://kent.dl.sourceforge.net/project/opencvlibrary/opencv-win/3.2.0/opencv-3.2.0-vc14.exe
+popd
+
+###############################################################################
+# Install.
+
+echo "Install git"
+../../download/Git-2.21.0-64-bit.exe
+# On last dialog, uncheck "Enable Git Credential Manager"
+echo "Install cmake"
+msiexec /a ../../download/cmake-3.14.4-win64-x64.msi
+
+###############################################################################
+# Build prerequisites.
+
+PREFIX=$PWD/out
+
+mkdir -p $PREFIX
+
+mkdir -p build
+pushd build
+
+
+if [ "$BUILD_OPENCV" == "true" ]
+then
+	unzip ../download/opencv-3.2.0.zip
+	mkdir -p opencv-3.2.0/build
+	pushd opencv-3.2.0/build
+	if [[ $OSTYPE == "windows" ]]
+	then
+		cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
+			-G "Visual Studio 14 2015 Win64" -T host=x64
+			#TODO Maybe do not need -T
+	else
+		cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+	fi
+	cmake --build . --config Release --target install
+	popd
+	rm -rf opencv-3.2.0
+else
+	echo "Install opencv-3.2.0-vc14.exe"
+fi
+
+
+unzip ../download/dlib-19.17.zip
+mkdir -p dlib-19.17/build
+pushd dlib-19.17/build
+if [[ $OSTYPE == "windows" ]]
+then
+	cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
+		-G "Visual Studio 14 2015 Win64" -T host=x64 -DUSE_AVX_INSTRUCTIONS=ON
+else
+	cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+fi
+cmake --build . --config Release --target install
+popd
+rm -rf dlib-19.17
+
+popd
+
+###############################################################################
+# Build app.
+
+mkdir -p build/face_landmark_remote_controller
+pushd build/face_landmark_remote_controller
+
+cmake ../../source/face_landmark_remote_controller/ \
+	-G "Visual Studio 14 2015 Win64"
+cmake --build . --config Release
+
+###############################################################################
+# Test.
+
+cp ../../download/shape_predictor_68_face_landmarks.dat.bz2 .
+bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
+if [[ $OSTYPE == "windows" ]]
+then
+	./Release/face_landmark_remote_controller.exe \
+		shape_predictor_68_face_landmarks.dat \
+		../../build/dlib-19.17/examples/faces/*.jpg
+else
+	./face_landmark_remote_controller \
+		shape_predictor_68_face_landmarks.dat \
+		../../build/dlib-19.17/examples/faces/*.jpg
+fi
+popd
+
+###############################################################################
---
+++ wscript
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+'''
+@author: Milos Subotic <milos.subotic.sm@gmail.com>
+@license: MIT
+
+@brief: Waf script just for distclean and dist commands.
+'''
+
+###############################################################################
+
+import os
+import fnmatch
+import shutil
+import datetime
+
+import waflib
+
+###############################################################################
+
+APPNAME = 'MWT_Source'
+
+top = '.'
+
+###############################################################################
+
+def recursive_glob(pattern, directory = '.'):
+	if pattern.startswith('/'):
+		for root, dirs, files in os.walk(directory, followlinks = True):
+			if root == directory:
+				for f in files:
+					if fnmatch.fnmatch('/' + f, pattern):
+						yield os.path.join(root, f)
+				for d in dirs:
+					if fnmatch.fnmatch('/' + d + '/', pattern):
+						yield os.path.join(root, d)
+	else:
+		for root, dirs, files in os.walk(directory, followlinks = True):
+			for f in files:
+				if fnmatch.fnmatch(f, pattern):
+					yield os.path.join(root, f)
+			for d in dirs:
+				if fnmatch.fnmatch(d + '/', pattern):
+					yield os.path.join(root, d)
+
+def collect_git_ignored_files():
+	for gitignore in recursive_glob('.gitignore'):
+		with open(gitignore) as f:
+			base = os.path.dirname(gitignore)
+
+			for pattern in f.readlines():
+				pattern = pattern.rstrip() # Remove new line stuff on line end.
+				for f in recursive_glob(pattern, base):
+					yield f
+
+###############################################################################
+
+def distclean(ctx):
+	for fn in collect_git_ignored_files():
+		if fn not in ['./download', './out']:
+			if os.path.isdir(fn):
+				shutil.rmtree(fn)
+			else:
+				os.remove(fn)
+
+def dist(ctx):
+	now = datetime.datetime.now()
+	time_stamp = '{:d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}'.format(
+		now.year,
+		now.month,
+		now.day,
+		now.hour,
+		now.minute,
+		now.second
+	)
+	ctx.arch_name = '../{}-{}.zip'.format(APPNAME, time_stamp)
+	ctx.algo = 'zip'
+	ctx.base_name = APPNAME
+	# Also pack git.
+	waflib.Node.exclude_regs = waflib.Node.exclude_regs.replace(
+'''
+**/.git
+**/.git/**
+**/.gitignore
+**/download
+**/download/**
+**/out
+**/out/**''', '')
+	# Ignore waf's stuff.
+	waflib.Node.exclude_regs += '\n**/.waf*'
+	
+###############################################################################
+
